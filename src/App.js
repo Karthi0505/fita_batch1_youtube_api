2db9fd74bcd70be3299357943f4daa6f6683d423
DhanushaRenganthan <imdhanusha11@gmail.com>
2024-04-11 21:01:00 +0530

    creating new files for sidebar and its css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import SideBar from './SideBar';
 
 function App() {
   return (
     <div className="App">
-      <h1> welcome purnima, Dhanushiya, priyanka </h1>
-      <header className="App-header">
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          React App
-        </a>
-        
-      </header>
+      <SideBar />
     </div>
   );
 }
---
+++ src/utils/constant.js
@@ -0,0 +1,17 @@
+export const categories= [
+    {name: "New"},
+    {name: "JS Mastery"},
+    {name: "Coding"},
+    {name: "React JS"},
+    {name: "Node JS"},
+    {name: "JS"},
+    {name: "Music"},
+    {name: "Education"},
+    {name: "Podcasts"},
+    {name: "Movie"},
+    {name: "Gaming"},
+    {name: "Sports"},
+    {name: "Fashion"},
+    {name: "Comedy"},
+    {name: "Gym"}
+];
