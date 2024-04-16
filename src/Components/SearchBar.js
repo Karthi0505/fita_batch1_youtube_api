4c18a9102b4df21939164537aedca9a4c9325a44
purni98n <purnima.n.1998@gmail.com>
2024-04-16 19:22:51 +0530

    used fetch api

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,30 +1,47 @@
-
 import React, { useState } from 'react';
 
-const SearchBar = () => {
-    const [searchVideo, setSearchVideo] = useState('');
+function SearchBar() {
+    const [query, setQuery] = useState('');
+    const [videos, setVideos] = useState([]);
 
-    const handleSearchChange = (event) => {
-        setSearchVideo(event.target.value);
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const url = 'https://youtube-v31.p.rapidapi.com/captions?part=snippet&videoId=M7FIvfx5J10';
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': '878619dbc4mshdfc6173e68b1585p1c26bbjsn8cb75c6bc728',
+                'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+            }
+        };
+
+        fetch(url, options)
+            .then(response => response.json())
+            .then(data => {
+
+                setVideos(data.items);
+            })
+            .catch(error => console.error('Error:', error));
     };
 
-    const handleSearch = () => {
-        console.log(searchVideo); 
-    };
 
     return (
-        <div>
+        <form onSubmit={handleSubmit}>
             <input
                 type="text"
-                placeholder="Search videos..."
-                value={searchVideo}
-                onChange={handleSearchChange}
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+                placeholder="Search YouTube"
             />
-            <button onClick={handleSearch}>Search</button>
-
-    
-        </div>
+            <button type="submit">Search</button>
+            <ul>
+                {videos.map((video) => (
+                    <li key={video.id.videoId}>{video.snippet.title}</li>
+                ))}
+            </ul>
+        </form>
     );
-};
+}
 
 export default SearchBar;
