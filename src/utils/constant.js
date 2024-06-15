f3702cd3a2b3c72333eca9ec04f42507c6de3a56
DhanushaRenganthan <imdhanusha11@gmail.com>
2024-06-15 15:40:26 +0530

    DHANUSHA - Sucessfully Completed the Project

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -26,29 +26,3 @@ export const demoVideoTitle = 'Default Video Title';
 export const demoChannelUrl = '/channel/defaultChannelId';
 export const demoChannelTitle = 'Default Channel Title';
 export const demoProfilePicture = 'https://example.com/default-profile-picture.jpg';
-
-// export const categories= [
-//     {name: "New" , icon:"./home.png"},
-//     {name: "JS Mastery"},
-//     {name: "Coding"},
-//     {name: "React JS"},
-//     {name: "Node JS"},
-//     {name: "JS"},
-//     {name: "Music"},
-//     {name: "Education"},
-//     {name: "Podcasts"},
-//     {name: "Movie"},
-//     {name: "Gaming"},
-//     {name: "Sports"},
-//     {name: "Fashion"},
-//     {name: "Comedy"},
-//     {name: "Gym"}
-// ];
-
-// export const demoVideoUrl = '/video/cV2gBU6hKfY';
-// export const demoThumbnailUrl = 'https://example.com/default-thumbnail.jpg';
-// export const demoVideoTitle = 'Default Video Title';
-// export const demoChannelUrl = '/channel/defaultChannelId';
-// export const demoChannelTitle = 'Default Channel Title';
-// export const demoProfilePicture = 'https://example.com/default-profile-picture.jpg';
-
